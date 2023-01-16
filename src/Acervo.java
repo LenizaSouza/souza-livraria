434221fd844ff0e7d77b225e7a4fc4f81a9c28d1
Leniza Souza <lenizarb@gmail.com>
2023-01-16 19:01:33 -0300

    metodos

--- src/Acervo.java
+++ src/Acervo.java
@@ -1,4 +1,71 @@
+import java.util.ArrayList;
 
 public class Acervo {
+private static ArrayList<LivroLivraria> listaLivros = new ArrayList<>();
 
+//metodo get
+	public static ArrayList<LivroLivraria> getListaLivros() {
+		return listaLivros;}
+
+//Adiciona um objeto na lista
+	static public void adicionar(LivroLivraria l) {
+		listaLivros.add(l);
+	}
+	
+// lista os dados de todos os objetos da lista
+	static public String listar() {
+		String saida = "";
+		int i = 1;
+		for(LivroLivraria l : listaLivros) {
+			saida += "\n===== Livro Nº " + (i++) + "=====\n";
+			saida += l.imprimir() + "\n";
+		}
+		
+		return saida;
+	}
+
+//pesquisar por genero
+	static public int pesquisar(String genero) {
+		int qtd = 0;
+		
+		for(LivroLivraria l : listaLivros) {
+			if(l.getGenero().equalsIgnoreCase(genero)) {
+				
+			}
+		}
+		return qtd;
+	} 
+
+//pesquisar por faixa de preco
+	static public int pesquisar(double vInicial, double vFinal) {
+		int qtd = 0;
+	
+		for(LivroLivraria l : listaLivros) {
+			if(l.getPreco() >= vInicial && l.getPreco() <= vFinal ) {
+				qtd++;
+		}
+	}
+		return qtd;
+}
+	
+//remove um livro pelo titulo
+	static public boolean remover(String titulo) {
+		for(LivroLivraria l : listaLivros) {
+			if(l.getTitulo().equalsIgnoreCase(titulo)) {
+				listaLivros.remove(0l);
+				return true;
+			}
+		}
+		return false;	
+	}
+	
+//total do acervo
+	static double calcularTotalAcervo() {
+		double total = 0;
+		
+		for(LivroLivraria l : listaLivros) {
+			total += l.getPreco();
+		}
+		return total;
+	}	
 }
