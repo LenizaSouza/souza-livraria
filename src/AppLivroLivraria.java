b233bebc8580bd052a85032339a43d7c5b2e455d
Leniza Souza <lenizarb@gmail.com>
2023-01-17 21:30:11 -0300

    menuLivraria

--- src/AppLivroLivraria.java
+++ src/AppLivroLivraria.java
@@ -1,4 +1,40 @@
+import java.util.Scanner;
 
 public class AppLivroLivraria {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		Scanner entradaString = new Scanner(System.in);
+		
+		int menu;
+		double vInicial, vFinal;
+		//referencia para a classe LivroLivraria
+		LivroLivraria objLivro;
+		
+		String titulo, autor, genero;
+		float preco;
+		int isbn;
+		
+		do {
+			exibirMenu();
+			menu = entrada.nextInt();
+			
+			switch(menu) {
+			
+			}
+			
+		}while(menu != 7);
+		
+	}//fim main
 	
+	static void exibirMenu() {
+		System.out.println("=======LIVRO LIVRARIA=======");
+		System.out.println("1 - CADASTRAR");
+		System.out.println("2 - LISTAR");
+		System.out.println("3 - EXCLUIR LIVRO");
+		System.out.println("4 - PESQUISAR POR GENERO");
+		System.out.println("5 - PESQUISAR POR FAIXA DE PREÇO");
+		System.out.println("6 - CALCULAR TOTAL DO ACERVO");
+		System.out.println("7 - SAIR");
+		System.out.println("---->Escolha uma opção");
+	}
 }
